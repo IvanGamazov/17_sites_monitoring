b9d53a7062dee92ff34a2e39b9eadb88ba6c2125
IvanGamazov <ivan.gamazov@gmail.com>
2016-10-14 13:26:55 +0300

    Скрипт проверяет статус домена и дату окончания его регистрации.

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,45 @@
-def load_urls4check(path):
-    pass
+import requests
+import os
+import whois
 
-def is_server_respond_with_200(url):
-    pass
+
+def load_urls4check(file_path):
+    urls = []
+    if os.path.exists(file_path):
+        with open(file_path) as urllist:
+            for url in urllist:
+                url.split()
+                urls.append(url[:-1])
+    return urls
+
+
+def get_server_respond(url):
+    response = requests.request('GET', 'http://'+url)
+    return response.status_code
+
+
+def get_results(urls):
+    for url in urls:
+        yield {
+            'domain': url,
+            'status': get_server_respond(url),
+            'exp_date': get_domain_expiration_date(url)
+               }
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois(domain_name)
+    expiration_date = domain_info.expiration_date
+    try:
+        exp_date = expiration_date[0].date()
+    except:
+        exp_date = expiration_date.date()
+    return exp_date
+
 
 if __name__ == '__main__':
-    pass
+    urlsfile = input('Путь до файла с URL-ами -->')
+    check_list = load_urls4check(urlsfile)
+    urls_info = get_results(check_list)
+    for url in urls_info:
+        print(url['domain'], url['status'], url['exp_date'])
