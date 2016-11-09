2d9180c548e7931e0ece56648114b347d6dfb34f
IvanGamazov <ivan.gamazov@gmail.com>
2016-11-09 14:38:10 +0300

    Исправлены ошибки, скрипт возвращает код после своей работы.
    Изменены имена некоторых переменных, поправлен парсинг url.

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,13 +1,24 @@
 import requests
-import os
 import whois
 import datetime
 import argparse
+from urllib.parse import urlparse, urljoin
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", nargs='?', help="Путь до файла с URL'ми для проверки'")
-args = parser.parse_args()
+parser.add_argument('-f', '--filepath',
+                    required=True,
+                    type=argparse.FileType('r'),
+                    help='the path to the file')
+
+
+def load_urls4check(text):
+    urls = []
+    url_list = text.split('\n')
+    for url in url_list:
+        parsed_url = urlparse(url)
+        urls.append(parsed_url.geturl().strip())
+    return urls
 
 
 def find_domain(url_string):
@@ -28,47 +39,34 @@ def find_domain(url_string):
     return domain
 
 
-def load_urls4check(file_path):
-    urls = []
-    if os.path.exists(file_path):
-        with open(file_path) as urllist:
-            for url in urllist:
-                url.split()
-                urls.append(url[:-1])
-    else:
-        urls = None
-    return urls
-
-
 def get_server_stat_code(url):
     try:
         stat_code = requests.get(url).status_code
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.ConnectionError or requests.exceptions.InvalidURL:
         stat_code = None
     return stat_code
 
 
-
 def get_server_status(url):
-    if 'http://' or 'https://' not in url:
-        status = get_server_stat_code('http://'+url)
+    try:
+        status = get_server_stat_code(urljoin('http://', url))
         if status is None:
-            status = get_server_stat_code('https://' + url)
-    else:
-        status = get_server_stat_code(+ url)
+            status = get_server_stat_code(urljoin('https://', url))
+    except requests.exceptions.InvalidURL:
+        status = None
     return status
 
 
 def get_full_info(urls):
-    url_list = []
+    url_info_list = []
     for url in urls:
         domain = find_domain(url)
-        url_list.append({
+        url_info_list.append({
             'domain': url,
             'status': get_server_status(url),
             'exp_date': get_domain_expiration_date(domain)
                })
-    return url_list
+    return url_info_list
 
 
 def expires_in_one_month(expire_date):
@@ -77,7 +75,10 @@ def expires_in_one_month(expire_date):
         try:
             expiration = expire_date.pop(0).date()
         except AttributeError:
-            expiration = expire_date.date()
+            try:
+                expiration = expire_date.date()
+            except AttributeError:
+                return None
         return expiration - month < datetime.datetime.now().date()
     else:
         return None
@@ -86,7 +87,7 @@ def expires_in_one_month(expire_date):
 def get_results(url_list):
     urls_fail = list(filter(lambda url: url['status'] != 200 or url['exp_date'] is None, url_list))
     urls_warning = list(filter(lambda url: url['status'] == 200 and expires_in_one_month(url['exp_date']), url_list))
-    urls_ok = list(filter(lambda url: url['status'] == 200 and  not expires_in_one_month(url['exp_date']), url_list))
+    urls_ok = list(filter(lambda url: url['status'] == 200 and not expires_in_one_month(url['exp_date']), url_list))
     return urls_fail, urls_warning, urls_ok
 
 
@@ -97,14 +98,11 @@ def get_domain_expiration_date(domain_name):
 
 
 if __name__ == '__main__':
-    if not args.file:
-        urlsfile = input('Путь до файла с URL-ами -->')
-    else:
-        urlsfile = args.file
-    check_list = load_urls4check(urlsfile)
-    if check_list is not None:
-        if len(check_list):
-            urls_info = get_full_info(check_list)
+    args = parser.parse_args()
+    urls4check = load_urls4check(args.filepath.read())
+    if urls4check is not None:
+        if len(urls4check):
+            urls_info = get_full_info(urls4check)
             fail_list, warn_list, ok_list = get_results(urls_info)
             print('URL не прошедшие проверку:')
             for fail in fail_list:
@@ -115,8 +113,9 @@ if __name__ == '__main__':
             print('URL прошли проверку:')
             for ok in ok_list:
                 print(ok['domain'], 'OK')
+            exit(0)
         else:
             print('Файл не содержит ссылок')
-    else:
-        print('Файл со ссылками не найден')
+            exit(1)
+
 
